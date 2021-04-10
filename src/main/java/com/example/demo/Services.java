0a636b23117aaedc9e418248e2de3b362c0ad55b
mjp81 <mjp81@LAPTOP-2N0EMT47>
2021-04-10 22:11:12 +0200

    changement update score, produit, manager

--- src/main/java/com/example/demo/Services.java
+++ src/main/java/com/example/demo/Services.java
@@ -5,6 +5,8 @@
  */
 package com.example.demo;
 
+import generated.PallierType;
+import generated.ProductType;
 import generated.World;
 import java.io.File;
 import java.io.FileInputStream;
@@ -31,9 +33,9 @@ public class Services {
         InputStream input;
         try {
             try {
-                System.out.println("in read"+path+username);
-                input = new FileInputStream(path+username + "-world.xml");
-                
+                System.out.println("coucou" + username);
+                input = new FileInputStream(path + username + "-world.xml");
+
             } catch (Exception ex) {
                 input = getClass().getClassLoader().getResourceAsStream("world.xml");
             }
@@ -47,14 +49,13 @@ public class Services {
         return world;
     }
 
-
     public void saveWorldToXml(World world, String username) {
         JAXBContext jaxbContext;
         try {
             jaxbContext = JAXBContext.newInstance(World.class);
             Marshaller march = jaxbContext.createMarshaller();
-            OutputStream output = new FileOutputStream(path  + username + "-world.xml");
-            System.out.println(username);
+            System.out.println("coucou2" + username);
+            OutputStream output = new FileOutputStream(path + username + "-world.xml");
             march.marshal(world, output);
         } catch (Exception ex) {
             System.out.println("Erreur écriture du fichier:" + ex.getMessage());
@@ -62,9 +63,117 @@ public class Services {
         }
     }
 
-   
     World getWorld(String username) {
-        System.out.println("getWorld()"+username);
+        World world = this.readWorldFromXml(username);
+        this.updateScore(world);
+        saveWorldToXml(world, username);
         return readWorldFromXml(username);
+    }
+
+    public Boolean updateProduct(String username, ProductType newproduct) {
+        // aller chercher le monde qui correspond au joueur
+        World world = getWorld(username);
+        // trouver dans ce monde, le produit équivalent à celui passé
+        // en paramètre
+        ProductType product = findProductById(world, newproduct.getId());
+        if (product == null) {
+            return false;
+        }
+
+        // calculer la variation de quantité. Si elle est positive c'est
+        // que le joueur a acheté une certaine quantité de ce produit
+        // sinon c’est qu’il s’agit d’un lancement de production.
+        int qtchange = newproduct.getQuantite() - product.getQuantite();
+        if (qtchange > 0) {
+            // soustraire de l'argent du joueur le cout de la quantité
+
+            double achat = product.getCout() * ((1 - Math.pow(product.getCroissance(), qtchange)) / (1 - product.getCroissance()));
+            world.setMoney(world.getMoney() - achat);
+
+            // achetée et mettre à jour la quantité de product 
+            product.setQuantite(newproduct.getQuantite());
+            product.setCout(newproduct.getCout());
+        } else {
+            product.setTimeleft(product.getVitesse());
+            // initialiser product.timeleft à product.vitesse
+            // pour lancer la production
+        }
+        // sauvegarder les changements du monde
+        saveWorldToXml(world, username);
+        return true;
+    }
+
+    private ProductType findProductById(World world, int id) {
+        for (ProductType product : world.getProducts().getProduct()) {
+            if (product.getId() == id) {
+                return product;
+            }
+        }
+        return null;
+    }
+
+    public Boolean updateManager(String username, PallierType newmanager) {
+        // aller chercher le monde qui correspond au joueur
+        World world = getWorld(username);
+        // trouver dans ce monde, le manager équivalent à celui passé
+        // en paramètre
+        PallierType manager = findManagerByName(world, newmanager.getName());
+        if (manager == null) {
+            return false;
+        }
+
+        // débloquer ce manager
+        manager.setUnlocked(true);
+        // trouver le produit correspondant au manager
+        ProductType product = findProductById(world, manager.getIdcible());
+        if (product == null) {
+            return false;
+        }
+        // débloquer le manager de ce produit
+        product.setManagerUnlocked(true);
+        // soustraire de l'argent du joueur le cout du manager
+        world.setMoney(world.getMoney() - manager.getSeuil());
+        // sauvegarder les changements au monde
+        saveWorldToXml(world, username);
+        return true;
+    }
+
+    private PallierType findManagerByName(World world, String name) {
+        for (PallierType manager : world.getManagers().getPallier()) {
+            if (manager.getName() == name) {
+                return manager;
+            }
+        }
+        return null;
+    }
+     private void updateScore(World world) {
+        long tempsEcoule = System.currentTimeMillis() - world.getLastupdate();
+        for (ProductType product : world.getProducts().getProduct()) {
+            if (!product.isManagerUnlocked()) {
+                if (product.getTimeleft() != 0 && tempsEcoule > product.getTimeleft()) {
+                    world.setMoney(world.getMoney() + product.getQuantite() * product.getRevenu());
+                    world.setScore(world.getScore() + product.getQuantite() * product.getRevenu());
+                } else {
+                    long tps = product.getTimeleft() - tempsEcoule;
+                    if (tps < 0) {
+                        product.setTimeleft(0);
+                    } else {
+                        product.setTimeleft(tps);
+                    }
+                }
+            } else {
+                //on divise  le temps écoulé par le temps de production pour obtenir la quatite fabriqué durant le temps écoulé 
+                double qtefabriquee = Math.floor(tempsEcoule / product.getVitesse());
+                world.setScore(world.getScore() + product.getQuantite() * product.getRevenu() * qtefabriquee);
+                world.setMoney(world.getMoney() + product.getQuantite() * product.getRevenu() * qtefabriquee);
+                long tps = product.getTimeleft() - tempsEcoule;
+                if (tps < 0) {
+                    product.setTimeleft(0);
+                } else {
+                    product.setTimeleft(tps);
+                }
+            }
+        }
+        world.setLastupdate(System.currentTimeMillis());
     }
 }
